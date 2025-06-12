563346cdd9b6f5952566b85d4e5582ada3d5750a
lo68961 <151887378+lo68961@users.noreply.github.com>
2025-06-12 19:40:28 -0300

    Update batalha_naval_novato.c

--- nivel-novato/batalha_naval_novato.c
+++ nivel-novato/batalha_naval_novato.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+   
+    int tabuleiro[5][5] = {0}; 
+
+    
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[i][1] = 1; 
+    }
+
+   
+    for (int j = 2; j <= 4; j++) {
+        tabuleiro[3][j] = 1; 
+    }
+
+    
+    printf("Coordenadas dos navios:\n\n");
+
+    printf("Navio Vertical:\n");
+    for (int i = 0; i < 3; i++) {
+        printf("(%d, %d)\n", i, 1);
+    }
+
+    printf("\nNavio Horizontal:\n");
+    for (int j = 2; j <= 4; j++) {
+        printf("(%d, %d)\n", 3, j);
+    }
+
+    return 0;
+}
 
